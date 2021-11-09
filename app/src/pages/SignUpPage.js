331610fcc521a50983cc43bb18b6f6ac3b83820a
Leon So <Leon_so7@hotmail.com>
2021-11-09 02:14:07 -0500

    Add form validation for sign up page

--- app/src/pages/SignUpPage.js
+++ app/src/pages/SignUpPage.js
@@ -5,53 +5,122 @@ import '../styles/pages/SignUpPage.css'
 
 //Sign up page
 class SignUpPage extends Component {
-  render() {
-    return (
-      <div class="wrapper">
-        <div id="signUpPageTitleDiv">
-          <h1>Sign up today!</h1>
-        </div>
 
-        {/* User registration form for sign up */}
-        <div id="signUpFormDiv">
-            <Form>
-                <Row>
-                    <Col>
-                        <Form.Group className="mb-3" controlId="formFirstName">
-                            <Form.Label>First Name</Form.Label>
-                            <Form.Control type="text" placeholder="First name"/>
+    constructor(props) {
+        super(props);
+        this.state = {
+            validated: false,
+            phoneNumberInputValue: ""
+        };
+    }
+
+    handleSubmit (event) {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+    
+        this.setState({
+            validated: true
+        });
+    };
+
+    handlePhoneNumberInput = (e) => {
+        var formatted = this.formatPhoneNumber(e.target.value);
+        this.setState({
+            phoneNumberInputValue: formatted
+        });
+      };
+
+    // Format phone number input to (###) ###-####
+    formatPhoneNumber(phoneNumber) {
+        // If input is empty (e.g., user deletes the phone number)
+        if (!phoneNumber) {
+            return phoneNumber;
+        }
+        
+        // Remove any non-digit characters (restrict user to only entering digits)
+        phoneNumber = phoneNumber.replace(/[^\d]/g, "");
+        
+        // Only apply formatting when the phone number is greater than just area code (i.e., more than 3 digits)
+        if (phoneNumber.length <= 3) {
+            return phoneNumber;
+        }
+
+        // Formatting for 4-6 digits entered
+        if (phoneNumber.length <= 6) {
+            return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+        }
+        
+        // Formatting for 7-10 digits entered
+        // Restrict input to 7 digits only (if user enters more, we will remove it)
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3,6)}-${phoneNumber.slice(6, 10)}`;
+    };
+
+    render() {
+        return (
+            <div class="wrapper">
+                <div id="signUpPageTitleDiv">
+                <h1>Sign up today!</h1>
+                </div>
+
+                {/* User registration form for sign up */}
+                <div id="signUpFormDiv">
+                    <Form noValidate validated={this.state.validated} onSubmit={this.handleSubmit.bind(this)}>
+                        <Row>
+                            <Col>
+                                <Form.Group className="mb-3" controlId="formFirstName">
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control type="text" placeholder="First name" pattern="[A-Za-z]" required/>
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                            <Form.Group className="mb-3" controlId="formLastName">
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Last name" required/>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Form.Group className="mb-3" controlId="formPhone">
+                            <Form.Label type="tel">Phone</Form.Label>
+                            <Form.Control 
+                                type="tel" 
+                                placeholder="Enter phone number" 
+                                required 
+                                onChange={(e) => this.handlePhoneNumberInput(e)} 
+                                value={this.state.phoneNumberInputValue}
+                                pattern="^\(\d{3}\)\s\d{3}-\d{4}"
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid 10-digit phone number.
+                            </Form.Control.Feedback>
                         </Form.Group>
-                    </Col>
-                    <Col>
-                    <Form.Group className="mb-3" controlId="formLastName">
-                            <Form.Label>Last Name</Form.Label>
-                            <Form.Control type="text" placeholder="Last name"/>
+                        <Form.Group className="mb-3" controlId="formEmail">
+                            <Form.Label>Email Address</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" required/>
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid email address.
+                            </Form.Control.Feedback>
                         </Form.Group>
-                    </Col>
-                </Row>
-                <Form.Group className="mb-3" controlId="formPhone">
-                    <Form.Label>Phone</Form.Label>
-                    <Form.Control type="tel" placeholder="Enter phone number"/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formEmail">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email"/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password"/>
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPostalCode">
-                    <Form.Label>Postal Code</Form.Label>
-                    <Form.Control type="text" placeholder="Enter postal code"/>
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        </div>
-    </div>
-    );
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" placeholder="Password" required/>
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPostalCode">
+                            <Form.Label>Postal Code</Form.Label>
+                            <Form.Control type="text" placeholder="Enter postal code" required/>
+                            <Form.Control.Feedback type="invalid">
+                                Please enter a valid postal code.
+                            </Form.Control.Feedback>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
+            </div>
+        );
   }
 }
 
