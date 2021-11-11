5a423101044f4388e64977aa7fa8dbd709a23fbf
Leon So <Leon_so7@hotmail.com>
2021-11-10 23:02:21 -0500

    Add search bar styling

--- app/src/components/SearchBar.js
+++ app/src/components/SearchBar.js
@@ -15,7 +15,7 @@ const SearchBar = () => (
                         state: { currentLat: 43.2610, currentLng: null }
                     }}
                     tabIndex="-1">
-                    <Button type="submit" variant="primary" aria-label="search"><FiSearch/></Button>
+                    <Button id="searchBtn" type="submit" variant="primary" aria-label="search"><FiSearch/></Button>
                 </Link>
             </div>
         </Form.Group>
