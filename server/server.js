d45733f45b63098ccd43cf1f491d22729e9b6093
Leon So <Leon_so7@hotmail.com>
2021-11-27 15:00:23 -0500

    Add express to node.js server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const http = require('http');
+const express = require("express");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
+app = express();
+
+app.get("/", function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, hostname, function () {
+	console.log("HTTP Listening on " + port);
 });
