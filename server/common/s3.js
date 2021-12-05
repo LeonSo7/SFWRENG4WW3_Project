f683215969960cec97715025c67b35994a7bdf78
Leon So <Leon_so7@hotmail.com>
2021-12-04 23:47:34 -0500

    Clean up image api and s3 util code

--- server/common/s3.js
+++ server/common/s3.js
@@ -1,46 +1,42 @@
 "use strict"
 
-require('dotenv').config()
-const S3 = require('aws-sdk/clients/s3')
-const fs = require('fs')
+require('dotenv').config();
+const S3 = require('aws-sdk/clients/s3');
+const fs = require('fs');
 
-const bucketName = process.env.S3_BUCKET_NAME
-const bucketRegion = process.env.S3_BUCKET_REGION
-const bucketAccessKey = process.env.AWS_ACCESS_KEY_ID
-const bucketSecretKey = process.env.AWS_SECRET_ACCESS_KEY
+const bucketName = process.env.S3_BUCKET_NAME;
+const bucketRegion = process.env.S3_BUCKET_REGION;
+const bucketAccessKey = process.env.AWS_ACCESS_KEY_ID;
+const bucketSecretKey = process.env.AWS_SECRET_ACCESS_KEY;
 
 const s3 = new S3({
     bucketRegion,
     bucketAccessKey,
     bucketSecretKey
-})
+});
 
 // Upload file to S3
-function uploadFile(file) {
-    const fileStream = fs.createReadStream(file.path)
+exports.uploadFile = function uploadFile(file) {
+    const fileStream = fs.createReadStream(file.path);
 
     const uploadParams = {
         Bucket: bucketName,
         Body: fileStream,
         Key: file.filename
-    }
+    };
 
-    const result = s3.upload(uploadParams).promise()
+    const result = s3.upload(uploadParams).promise();
 
-    return result
+    return result;
 
-}
-
-exports.uploadFile = uploadFile
+};
 
 // Get file from S3
-function getFileStream(fileKey) {
+exports.getFileStream = function getFileStream(fileKey) {
     const downloadParams = {
         Key: fileKey,
         Bucket: bucketName
-    }
+    };
 
-    return s3.getObject(downloadParams).createReadStream()
-}
-
-exports.getFileStream = getFileStream
+    return s3.getObject(downloadParams).createReadStream();
+};
--- server/routes/images_router.js
+++ server/routes/images_router.js
@@ -7,29 +7,29 @@ const multer = require("multer");
 const upload = multer({dest: './uploads'});
 const { uploadFile, getFileStream } = require('../common/s3');
 
-const fs = require('fs')
-const util = require('util')
+const fs = require('fs');
+const util = require('util');
 
-const unlinkFile = util.promisify(fs.unlink)
+const unlinkFile = util.promisify(fs.unlink);
 
 api.get('/images/:key', (req, res) => {
-  const key = req.params.key
-  const readStream = getFileStream(key)
+  const key = req.params.key;
+  const readStream = getFileStream(key);
 
-  readStream.pipe(res)
+  readStream.pipe(res);
 })
 
 api.post('/images', upload.single('image'), async (req, res) => {
-  const file = req.file
+  const file = req.file;
   // Use the path and filename for the s3 bucket. Also store the filename in the db so we can easily get it later
   
   // Wait for upload to be successful 
-  const result = await uploadFile(file)
+  const result = await uploadFile(file);
 
   // Delete the file after it is uploaded to S3
-  await unlinkFile(file.path)
+  await unlinkFile(file.path);
 
-  res.send(JSON.stringify({imagePath: `/images/${result.Key}`}))
+  res.send(JSON.stringify({imagePath: `/images/${result.Key}`}));
 })
 
 module.exports = api;
